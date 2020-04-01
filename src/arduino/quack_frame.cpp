d72f1be6f16a34df23e59bf51ec81cf70d98c067
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2020-04-01 18:23:37 -0300

    Remove debugging on QuackFrame deserialize

--- src/arduino/quack_frame.cpp
+++ src/arduino/quack_frame.cpp
@@ -7,21 +7,15 @@ QuackFrame::QuackFrame() : checksum{0}, commandCode{COMMAND_NONE},
     // no-op
 }
 
-#include <cstdio>
 const bool
 QuackFrame::deserialize(const u8* const str, const u16 len, FastCRC16* CRC16) {
-    for(u16 i = 2; i < len; i++) printf("%d, ", str[i]);
-    putchar('\n');
     
     checksum = CRC16->ccitt(
         str + 2, // skip previous checksum
         len - 2
     );
 
-    u16 sentChecksum = DESERIALIZE_U16(str);
-    printf("Checksum %d", sentChecksum);
-
-    if(checksum != sentChecksum) {
+    if(checksum != DESERIALIZE_U16(str)) {
         return false; // wrong checksum
     }
 
