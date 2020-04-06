b805916c4a09cffb7851cca45b92b696fa0f92e5
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2020-04-06 00:53:07 -0300

    Change QuackHID debugging to Serial

--- src/arduino/src/quack_hid.h
+++ src/arduino/src/quack_hid.h
@@ -17,19 +17,17 @@
 
 #else
 
-// #include <cstdio>
-
 struct HIDSubDescriptor {
     HIDSubDescriptor(const u8* keyboardDescriptor, u32 len) {}
 };
 
 struct HID {
     void AppendDescriptor(HIDSubDescriptor* h) { 
-        DEBUGGING_PRINTF("[HID] Appending descriptor.\n");
+        DEBUGGING_PRINT("[HID] Appending descriptor.\n");
     }
 
     void SendReport(u8 id, const void* data, u32 len) {
-        DEBUGGING_PRINTF("[HID] Sending report.\n");
+        DEBUGGING_PRINT("[HID] Sending report.\n");
     }
 };
 
